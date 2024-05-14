63d93aa3424b560738d3533358346070e7ba1f4a
Yuri <49888082+YuriRDev@users.noreply.github.com>
2024-05-14 20:12:00 -0300

    Reservation Statiosn

--- src/main.c
+++ src/main.c
@@ -4,103 +4,133 @@
 
 #include "Instruction.h"
 
-#define MAX_INSTR_LENGTH 25 // Maximum length of each instruction
+#define MAX_INSTR_LENGTH 25  // Maximum length of each instruction
 #define MAX_INSTRUCTIONS 100 // Maximum number of instructions
-#define REG_AMOUNT 16 // Number of FP registers
-#define STATIONS_AMOUNT 5 // Number of reservation stations - 3 adders and 2 multipliers
-#define BUFFERS_AMOUNT 4 // Number of load/store buffers - 2 for each
+#define REG_AMOUNT 16        // Number of FP registers
+#define STATIONS_AMOUNT 7    // Number of reservation stations - 2 adders, 1 multiplier, 2 load, 2 store
 
 int clock = 0;
 
-typedef enum {
-    Adder,
-    Multiplier
+typedef enum
+{
+    Adder,      // 0
+    Multiplier, // 1
+    Load,       // 2
+    Store       // 3
 } StationType;
 
-typedef struct {
-    bool busy;
+typedef struct
+{
     bool ready;
+    bool busy;
     StationType type;
     Instruction in;
-} ReservationStation;
+} Station;
 
-typedef enum {
-    Load,
-    Store
-} BufferType;
-
-typedef struct {
-    bool busy;
-    BufferType type;
-    Instruction in;
-} LoadStoreBuffer; static es(int registers[REG_AMOUNT], ReservationStation stations[STATIONS_AMOUNT], LoadStoreBuffer buffers[BUFFERS_AMOUNT]) {
-    int register[REGISTER_AMOUT] = {0}
-
-    for (int i = 0; i < STATIONS_AMOUNT; i++) {
-        if (i <= 2) {
-            stations[i] = { 0,0,Adder,0 };       
-        } else {
-            stations[i] = { 0,0,Multiplier,0 };       
+/**
+ * Se retornar -1 é por que está busy.
+ */
+int findNonBusyStation(Station station[STATIONS_AMOUNT], StationType type)
+{
+    for (int i = 0; i < STATIONS_AMOUNT; i++)
+    {
+        if (type == station[i].type)
+        {
+            if (!station[i].busy)
+                return i;
         }
     }
+    return -1;
+}
 
-    for (int i = 0; i < BUFFERS_AMOUNT; i++) {
-        if (i <= 1) {
-            buffers[i] = { 0,Load,0 }
-        } else {
-            buffers[i] = { 0,Store,0 }
-        }        
-    }
-};
-
-int readInstructions(const char* filePath, char instructions[][MAX_INSTR_LENGTH]) {
+int readInstructions(const char *filePath, char instructions[][MAX_INSTR_LENGTH])
+{
     FILE *fptr;
     fptr = fopen(filePath, "r");
 
-    if (fptr == NULL) {
+    if (fptr == NULL)
+    {
         printf("Error opening file.\n");
         exit(0);
     }
 
     int count = 0;
-    while(fgets(instructions[count], MAX_INSTR_LENGTH, fptr) != NULL && count < MAX_INSTRUCTIONS) { count++; }
+    while (fgets(instructions[count], MAX_INSTR_LENGTH, fptr) != NULL && count < MAX_INSTRUCTIONS)
+    {
+        count++;
+    }
 
     fclose(fptr);
     return count;
 };
 
-int main(void) {
+StationType getDispatchTarget(Instruction inst)
+{
+    switch (inst.operation)
+    {
+    case ADD:
+    case SUB:
+        return Adder;
+    case LW:
+        return Load;
+    case SW:
+        return Store;
+    case MUL:
+    case DIV:
+        return Multiplier;
+    default:
+        printf("ERROR - GetDispatchTarget, unreachable!");
+        exit(0);
+    }
+}
+
+int main(void)
+{
     char inputInstructions[MAX_INSTRUCTIONS][MAX_INSTR_LENGTH];
     const char inputPath[] = "./input/input_01.txt"; // Change path if needed.
 
-    int instructionAmount = readInstructions(inputPath, inputInstructions);      
+    int instructionAmount = readInstructions(inputPath, inputInstructions);
 
-    if (instructionAmount == -1) {
+    if (instructionAmount == -1)
+    {
         printf("\033[1;31m Failed to read input file.\n");
         return 1;
     }
 
     Instruction instructionsList[instructionAmount];
 
-    for (short i = 0; i < instructionAmount; i++) {
+    for (short i = 0; i < instructionAmount; i++)
+    {
         instructionsList[i] = parseInstruction(inputInstructions[i]);
     }
 
-    int registers[REG_AMOUNT];
-    ReservationStation reservationStations[STATIONS_AMOUNT];
-    LoadStoreBuffer loadStoreBuffers[BUFFERS_AMOUNT];
+    /** Initialize Structures */
+    int registers[REG_AMOUNT] = {0};
+    Station reservationStations[STATIONS_AMOUNT] = {
+        {0, 0, Adder, 0}, {0, 0, Adder, 0}, {0, 0, Multiplier, 0}, {0, 0, Load, 0}, {0, 0, Load, 0}, {0, 0, Store, 0}, {0, 0, Store, 0}};
 
-    initializeStructures(registers, reservationStations, LoadStoreBuffer);
-
-    int i = 0;
-
-    while(1) {
-        debugInstruction(instructionsList[i++]);
-
-        if (i == instructionAmount) {
+    int currentInstructionPos = 0;
+    while (1)
+    {
+        if (currentInstructionPos == instructionAmount)
             break;
+        Instruction currentInstruction = instructionsList[currentInstructionPos];
+        // debugInstruction(currentInstruction);
+        StationType stationType = getDispatchTarget(instructionsList[currentInstructionPos++]);
+        int targetStationIndex = findNonBusyStation(reservationStations, stationType);
+
+        if (targetStationIndex == -1)
+        {
+            printf("TODO - Stations are full, but it's not implemented");
+            exit(0);
         }
 
+        reservationStations[targetStationIndex].busy = 1;
+        reservationStations[targetStationIndex].in = currentInstruction;
+        printf("Reservation Station: \n");
+        printf("Busy: %d", reservationStations[targetStationIndex].busy);
+        debugInstruction(currentInstruction);
+        printf("[Index]: %d\n\n", targetStationIndex);
         clock += 1;
     }
 
@@ -111,6 +141,6 @@ int main(void) {
  * Temos 2 tipos de estacao de reserva:
  * 1. 4x de operacao inteira
  * 2. 2x de ponto flutuante
- * 
- * Eles armazenam instrucoes 
-*/
+ *
+ * Eles armazenam instrucoes
+ */
